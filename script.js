93accd3ed47d3563b21b64b649de9c44cdd5c517
fahe94 <fabian.herrmann@tum.de>
2024-12-03 11:58:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function checkPassword() {
+  const password = document.getElementById('password').value;
+  const correctPassword = "geheim123"; // Hier dein Passwort festlegen
 
+  if (password === correctPassword) {
+    document.getElementById('login').style.display = 'none';
+    document.getElementById('protected-content').style.display = 'block';
+  } else {
+    document.getElementById('error').textContent = 'Falsches Passwort. Bitte versuche es erneut.';
+  }
+}
